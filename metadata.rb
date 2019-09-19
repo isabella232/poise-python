dbff86ded176286072a444df6528b24545a9804d
Alan Sebastian <asebastian2@gmail.com>
2019-09-19 12:04:04 -0400

    Add a metadata.rb

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'poise-python'
+version          '1.7.2'
+
+depends 'chef', '>= 12.16', '< 15'
+depends 'halite', '~> 1.0'
+depends 'poise', '~> 2.7'
+depends 'poise-languages', '~> 2.0'
